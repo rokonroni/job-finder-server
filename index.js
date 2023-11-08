aedb960444b201e6d6cbc8ae53762a379201b84a
rokonroni <rokonuzzamanroni@gmail.com>
2023-11-08 06:24:46 +0600

    GET All my jobs API added

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173' 
+}));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8sb7n8j.mongodb.net/?retryWrites=true&w=majority`;
@@ -25,12 +27,41 @@ async function setupServer() {
     console.log("Connected to MongoDB");
 
     const AllJobsCollection = client.db("jobFinderDB").collection("allJobs");
+    const AllApplyJobsCollection = client.db("jobFinderDB").collection("applyJobs");
 
-     app.get("/allJobs", async (req, res) => {
+    app.get("/allJobs", async (req, res) => {
       const cursor = AllJobsCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
+    app.put("/jobUpdate/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateJob = req.body;
+      const newProduct = {
+        $set: {
+          pictureUrl: updateJob.pictureUrl,
+          jobTitle: updateJob.jobTitle,
+          name: updateJob.name,
+          userEmail: updateJob.userEmail,
+          jobCategory: updateJob.jobCategory,
+          salaryRange: updateJob.salaryRange,
+          userImage: updateJob.userImage,
+          jobDescription: updateJob.jobDescription,
+          jobPostingDate: updateJob.jobPostingDate,
+          applicationDeadline: updateJob.applicationDeadline,
+          jobApplicantsNumber: updateJob.jobApplicantsNumber,
+        },
+      };
+      const result = await AllJobsCollection.updateOne(
+        filter,
+        newProduct,
+        options
+      );
+      res.send(result);
+    });
+
     app.get("/jobDetails/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -43,17 +74,28 @@ async function setupServer() {
       const result = await AllJobsCollection.insertOne(newJobs);
       res.send(result);
     });
+    app.post("/applyJob", async (req, res) => {
+      const applyJob = req.body;
+      const result = await AllApplyJobsCollection.insertOne(applyJob);
+      res.send(result);
+    });
 
-    app.get('/myJobs', async (req, res)=>{
-      console.log(req.query.email);
+    app.get("/myJobs", async (req, res) => {
       let query = {};
-      if(req.query?.email){
-        query= {email: req.query.email}
+      if (req.query?.email) {
+        query = { userEmail: req.query.email };
       }
-      const cursor = AllJobsCollection.find();
+      const cursor = AllJobsCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
-    })
+    });
+
+    app.delete("/myJobs/job/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await AllJobsCollection.deleteOne(query);
+      res.send(result);
+    });
 
     app.get("/", (req, res) => {
       res.send("Job Finder server is running");
